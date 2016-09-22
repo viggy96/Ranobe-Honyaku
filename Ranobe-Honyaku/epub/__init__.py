40e4a944ce71cc3b1d3353699f34153b089cdd8e
viggy96 <vigneshb.hp@gmail.com>
2016-09-22 13:38:37 -0400

    Initial commit.

--- Ranobe-Honyaku/epub/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-epub = Blueprint("epub", __name__, subdomain="epub")
-
-
-@epub.route(rule="/")
-def root():
-    return render_template("epub/epub_generator.html")
--- Ranobe-Honyaku/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# Reads our intial setup file
-with open("./setup.json") as file:
-    setup_file = json.load(file)
-
-# I need to make this better
-apps = []
